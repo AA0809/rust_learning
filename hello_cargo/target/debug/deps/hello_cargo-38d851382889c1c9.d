425b53c16fe56a70a1440bc2623d5fddbd68d6ed
aagar <aviral.ag.08@gmail.com>
2026-06-04 22:09:07 +0100

    hello_cargo, learning how to use cargo to manage projects

---
+++ hello_cargo/target/debug/deps/hello_cargo-38d851382889c1c9.d
@@ -0,0 +1,5 @@
+C:\Users\aagar\Coding\rust_learning\hello_cargo\target\debug\deps\hello_cargo-38d851382889c1c9.d: src\main.rs
+
+C:\Users\aagar\Coding\rust_learning\hello_cargo\target\debug\deps\libhello_cargo-38d851382889c1c9.rmeta: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\aagar\Coding\rust_learning\hello_cargo\target\debug\hello_cargo.exe: C:\Users\aagar\Coding\rust_learning\hello_cargo\src\main.rs
